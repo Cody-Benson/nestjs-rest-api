b3eb31e7780fd4bf95a27d693ff6581a1a753ac2
Cody-Benson <codyobenson@gmail.com>
2025-05-22 14:50:23 -0500

    Added .env config file for database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,28 @@ import { AppService } from './app.service';
 import { UserModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 @Module({
-  imports: [UserModule,TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'password',
-    database: 'postgres',
-    entities: [User],
-    synchronize: true,
-    autoLoadEntities:true,
-  })],
+  imports: [
+    ConfigModule.forRoot({}),
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DATABASE_TYPE') as 'postgres',
+        host: configService.get<string>('DATABASE_HOST',),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [User],
+        synchronize: configService.get<boolean>('DATABASE_SYNCHRONIZE'), // Set to false in production
+        autoLoadEntities: configService.get<boolean>('DATABASE_AUTOLOAD_ENTITIES'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
